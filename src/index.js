a2a406520e3e0203242ff0c3db079cb1f462ec24
Prashant <meprashanttyagi2000@gmail.com>
2024-09-17 14:34:13 +0530

    Updating1 project

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import dotenv from "dotenv";
+import connectDB from "./db/database.js";
+
+dotenv.config({
+    path: "./env"
+});
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+;(async ()=>{
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)  
+      app.on("error", () => {
+        console.log("Error:- ", error);
+        throw error
+      });
+
+      app.listen(process.env.PORT, () => {
+        console.log(`App is serving on http://localhost${process.env.PORT}`);
+      });
+    } 
+    
+    catch (error) {
+        console.log("Error in connecting to DB:- ", error.message);    
+        throw error
+    }
+})()
+
+*/
