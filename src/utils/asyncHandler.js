d45c5c1a85a0d6fc277cce1046e30701679d21ee
Prashant <meprashanttyagi2000@gmail.com>
2024-09-17 15:58:43 +0530

    Upadtion2

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+// 1. promises wala tareeka
+
+const asyncHandler = (requestHandler) => {
+    (request, response, next) => {
+        Promise.resolve(requestHandler(request, response, next)).catch((error) => next(error))
+    }
+}
+
+
+
+
+/* 2. try catch wala tareeka
+const asyncHandler = (func) => async (request, response, next) => {
+    try {
+        await func(request, response, next)
+    } 
+    
+    catch (error) {
+        response.status(error.code || 500).json({
+            message: error.message,
+            success: false,
+        })    
+    }
+}
+*/
+
+export { asyncHandler }
